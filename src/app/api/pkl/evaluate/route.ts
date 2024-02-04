edac33e4fd6d549b61e1ce002d6ff11bc485eabf
Jason Gwartz <jason@gwartz.me>
2024-02-04 23:51:43 +0000

    Add select for output format

--- src/app/api/pkl/evaluate/route.ts
+++ src/app/api/pkl/evaluate/route.ts
@@ -1,9 +1,19 @@
 import { evaluate } from "@pkl-community/pkl-eval";
 
 export async function POST(req: Request) {
-  const { pklInput } = (await req.json()) as { pklInput: string };
+  const { pklInput, outputFormat } = (await req.json()) as {
+    pklInput: string;
+    outputFormat: any;
+  };
 
-  return Response.json({
-    output: await evaluate(pklInput, {}),
-  });
+  try {
+    return Response.json({
+      output: await evaluate(pklInput, { format: outputFormat ?? "pcf" }),
+    });
+  } catch (err) {
+    return Response.json(
+      { error: err instanceof Error ? err.message : JSON.stringify(err) },
+      { status: 500 }
+    );
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
-import { Box, Columns, Container, Heading, Hero } from "react-bulma-components";
-import React from "react";
+import {
+  Box,
+  Columns,
+  Container,
+  Heading,
+  Hero,
+  Form,
+} from "react-bulma-components";
+import React, { useEffect } from "react";
 import { githubLight } from "@uiw/codemirror-theme-github";
 
 import CodeMirror from "@uiw/react-codemirror";
@@ -12,27 +19,57 @@ anObject {
   x = 1
   y = 2
 }
-
-output {
-  renderer = new JsonRenderer {}
-}
 `;
+
+const outputOptions = [
+  "JSON",
+  "Jsonnet",
+  "Pcf",
+  "Plist",
+  "Properties",
+  "TextProto",
+  "XML",
+  "YAML",
+] as const;
+
+type OutputOption = (typeof outputOptions)[number];
 
 export default function Home() {
   const [userInputCode, setUserInputCode] = React.useState(defaultText);
   const [evaluatedOutputCode, setEvaluatedOutputCode] = React.useState(
     "// Change the Pkl code on the left to see some output"
   );
+  const [outputFormat, setOutputFormat] = React.useState<OutputOption>("JSON");
+
+  useEffect(() => {
+    const refreshPklOutput = async () => {
+      const response = await fetch("/api/pkl/evaluate", {
+        method: "POST",
+        body: JSON.stringify({
+          pklInput: userInputCode,
+          outputFormat: outputFormat.toLowerCase(),
+        }),
+      });
+      if (response.ok) {
+        setEvaluatedOutputCode((await response.json()).output);
+      } else {
+        setEvaluatedOutputCode((await response.json()).error);
+      }
+    };
+
+    refreshPklOutput();
+  }, [outputFormat, userInputCode]);
+
   const onUserInputChange = React.useCallback(async (val: string) => {
     setUserInputCode(val);
-    const response = await fetch("/api/pkl/evaluate", {
-      method: "POST",
-      body: JSON.stringify({
-        pklInput: val,
-      }),
-    });
-    setEvaluatedOutputCode((await response.json()).output);
   }, []);
+
+  const onOutputFormatChange = React.useCallback(
+    async (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setOutputFormat(event.target.value as OutputOption);
+    },
+    []
+  );
 
   return (
     <main>
@@ -55,6 +92,7 @@ export default function Home() {
                   extensions={[]}
                   theme={githubLight}
                   onChange={onUserInputChange}
+                  autoFocus
                 />
               </Box>
             </Container>
@@ -62,11 +100,21 @@ export default function Home() {
           <Columns.Column>
             <Container>
               <Box>
+                <Form.Select onChange={onOutputFormatChange}>
+                  {outputOptions.map((opt) => (
+                    <option value={opt.toLowerCase()} key={opt}>
+                      {opt}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Box>
+              <Box>
                 <CodeMirror
                   value={evaluatedOutputCode}
                   extensions={[]}
                   theme={githubLight}
                   onChange={onUserInputChange}
+                  editable={false}
                 />
               </Box>
             </Container>
