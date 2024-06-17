8777485ca50c2b640960a8762a5f9ba4087966a6
Jason Gwartz <jason@gwartz.me>
2024-06-17 16:17:23 +0100

    Add CORS header (any origin)

--- src/app/api/pkl/evaluate/route.ts
+++ src/app/api/pkl/evaluate/route.ts
@@ -1,27 +1,54 @@
 import { evaluate } from "@pkl-community/pkl-eval";
 
-export async function POST(req: Request) {
-  const { pklInput, outputFormat } = (await req.json()) as {
-    pklInput: string;
-    outputFormat: any;
-  };
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST",
+} as const;
 
+export async function OPTIONS() {
+  return new Response("", {
+    headers,
+  });
+}
+
+export async function POST(req: Request) {
+  let pklInput: string;
+  let outputFormat: string;
   try {
-    return Response.json({
-      output: await evaluate(pklInput, {
-        format: outputFormat ?? "pcf",
-        allowedResources: ["package:"],
-        allowedModules: ["pkl:", "repl:"],
-        cache: {
-          enabled: true,
-          directory: "/tmp/pkl",
-        },
-      }),
-    });
+    const body = (await req.json()) as {
+      pklInput: string;
+      outputFormat: string;
+    };
+    pklInput = body.pklInput;
+    outputFormat = body.outputFormat;
   } catch (err) {
     return Response.json(
       { error: err instanceof Error ? err.message : JSON.stringify(err) },
-      { status: 500 }
+      { status: 400, headers }
+    );
+  }
+
+  try {
+    return Response.json(
+      {
+        output: await evaluate(pklInput, {
+          format: (outputFormat as any) ?? "pcf",
+          allowedResources: ["package:"],
+          allowedModules: ["pkl:", "repl:"],
+          cache: {
+            enabled: true,
+            directory: "/tmp/pkl",
+          },
+        }),
+      },
+      {
+        headers,
+      }
+    );
+  } catch (err) {
+    return Response.json(
+      { error: err instanceof Error ? err.message : JSON.stringify(err) },
+      { status: 500, headers }
     );
   }
 }
